044a9feb76263ad713e46006d45fac488393665d
Karen <kngs@ic.ufal.br>
2020-05-24 19:01:26 -0300

    feat: 🎸 add axios and request get method

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
-  return <div>Hello, World!</div>;
+  const [user, setUser] = useState("");
+
+  function handleSearch() {
+    axios
+      .get(`https://api.github.com/users/${user}/repos`)
+      .then((response) => console.log(response.data));
+  }
+
+  return (
+    <>
+      <input
+        className="inputUser"
+        placeholder="User"
+        value={user}
+        onChange={(e) => setUser(e.target.value)}
+      />
+      <button type="button" onClick={handleSearch}>
+        Search
+      </button>
+    </>
+  );
 }
 
 export default App;
