3d7b4f831eb0ef45d62f5cdc03c16e63fa72175c
Karen <kngs@ic.ufal.br>
2020-05-26 13:26:31 -0300

    feat: 🎸 add react-router-dom

---
+++ src/Repositories.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function Respositories() {
+
+
+  return (
+    <>
+      <h1>Repositories</h1>
+    </>
+  );
+}
+
+export default Respositories;
